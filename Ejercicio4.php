6fb2cfdd5361914504c262cbbd5023899e0e28c5
uo270115 <uo270115@uniovi.es>
2020-11-29 16:06:36 +0100

    Ejercicio4

---
+++ Ejercicio4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ejercicio4</title>
+	<link rel="stylesheet" href="Ejercicio4.css"/>
+</head>
+<body>
+<h1>DigitalCOVIDNews</h1>
+    <section>
+        <?php
+            include_once("Ejercicio4Codigo.php");
+            $news = new News();
+        ?>
+    </section>
+
+    <section>
+        <form action="" method="post">
+            <div id="noticias"><?php echo $news->mostrar();?></div>
+        </form> 
+    </section>
+
+    <a href="https://validator.w3.org/check?uri=referer">
+        <img src="HTML5.png" alt=" HTML5 Válido!" /></a>
+    
+    <a href=" http://jigsaw.w3.org/css-validator/check/referer">
+        <img src="CSS3.png" alt="CSS Válido!" height="63" width="64"/></a>
+</body>
+</html>
---
+++ Ejercicio4Codigo.php
@@ -0,0 +1,44 @@
+<?php
+    class News{
+
+        public function __construct(){
+
+            $fechaHoy = date_create(); // DateTime
+            $fechaHoy->sub(new DateInterval('P7D'));
+            setlocale(LC_TIME, "es-ES");
+            $fechaHaceUnaSemana = strftime("%V,%G,%Y", date_timestamp_get($fechaHoy));
+    
+            $url = "https://gnews.io/api/v4/search?everything?&q=COVID&lang=es&country=es&from=" . $fechaHaceUnaSemana . "&sortBy=publishedAt&token=2956d20a0e0f5189c281f15099a359ee";
+    
+            $datos = file_get_contents($url);
+            $json = json_decode($datos, true);
+
+            $articles = $json["articles"];
+            for($i = 0; $i < count($articles); $i++){
+                $noticia = "";
+                $noticia .= "<article>";
+                $noticia .= "<h2>" . $articles[$i]["title"] . "</h2>";
+                $noticia .= "<p>" . $articles[$i]["description"] . "</p>";
+                $noticia .= "<p>" . $articles[$i]["content"] . "</p>";
+                $noticia .= "<img src='" . $articles[$i]["image"] . "' alt='Foto representativa de la noticia' height='500' width='100%'/>";
+                $noticia .= "<p>Fuente de información: " . $articles[$i]["source"]["name"] . "</p>";
+                $noticia .= "<p>Fecha de la punlicación: " . $articles[$i]["publishedAt"] . "</p>";
+                $noticia .= "<a href='" . $articles[$i]["url"] . "'>Ver la noticia completa</a>";
+                $noticia .= "</article>";
+
+                if(isset($_SESSION["mostrar"])){
+                    $_SESSION["mostrar"] .= $noticia;
+                }else{
+                    $_SESSION["mostrar"] = $noticia;
+                }
+            }
+        }
+
+        public function mostrar(){
+            if(isset($_SESSION["mostrar"])){
+                return $_SESSION["mostrar"];
+            }
+        }
+
+    }
+?>
